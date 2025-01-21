00d32c25e5cb1ef664f394e2175525470f10357f
NotTheStas <113777167+NotTheStas@users.noreply.github.com>
2025-01-21 17:51:38 +1000

    fix

--- task2/mainapp/views.py
+++ task2/mainapp/views.py
@@ -1,22 +1,22 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.csrf import csrf_exempt
-import json
 from .models import Product
 
 def get_all_products(request):
     if request.method == 'GET':
         products = Product.objects.all()
-        data = [
-            {"id": product.id, "name": product.name, "description": product.description}
-            for product in products
-        ]
-        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+        data = []
+        for product in products:
+            data.append({"id": product.id, "name": product.name, "description": product.description})
+        return JsonResponse(data, safe=False)
+
 def get_product_by_id(request, product_id):
     if request.method == 'GET':
-        product = get_object_or_404(Product, id=product_id)
+        product = Product.objects.get(id=product_id)
         data = {"id": product.id, "name": product.name, "description": product.description}
-        return JsonResponse(data, json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(data)
+
 @csrf_exempt
 def create_product(request):
     if request.method == 'POST':
